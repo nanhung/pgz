1dbf712102f37edbd9195751d227d4710319c58f
Nan-Hung Hsieh <d99622005@ntu.edu.tw>
2022-01-13 22:44:31 -0800

    update

--- intro.py
+++ intro.py
@@ -1,13 +1,20 @@
 import pgzrun
 
 alien = Actor('alien')
-alien.pos = 100, 56
+alien.pos = 56, 56
+#alien.topright = 0, 10
 
 WIDTH = 500
 HEIGHT = alien.height + 20
 
+
 def draw():
     screen.clear()
     alien.draw()
+
+def update():
+    alien.left += 2
+    if alien.left > WIDTH:
+        alien.right = 0
 
 pgzrun.go()
