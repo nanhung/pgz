2e74444a50996aa947084c0fab0e2ad996bc1856
Nan-Hung Hsieh <d99622005@ntu.edu.tw>
2022-01-18 20:20:02 -0800

    alien

--- alien.py
+++ alien.py
@@ -1,8 +1,8 @@
 import pgzrun
 
 alien = Actor('alien')
-alien.pos = 56, 56
-#alien.topright = 0, 10
+#alien.pos = 56, 56
+alien.topright = 0, 10
 
 WIDTH = 500
 HEIGHT = alien.height + 20
@@ -16,5 +16,11 @@ def update():
     alien.left += 4
     if alien.left > WIDTH:
         alien.right = 0
+
+def on_mouse_down(pos):
+    if alien.collidepoint(pos):
+        print("Eek!")
+    else:
+        print("You missed me!")
 
 pgzrun.go()
